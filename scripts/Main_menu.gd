03b298b035f14f046923d5a4806e1e5312a8790a
Sayem <sayem@caffebd.org>
2024-07-31 17:26:39 +0600

    button

--- scripts/Main_menu.gd
+++ scripts/Main_menu.gd
@@ -3,15 +3,13 @@ extends Node2D
 func _ready():
 	pass
 
-func _on_game_button_pressed():
+func _on_level_button_pressed():
 	GlobalVars.game_type = "level_game"
 	get_tree().change_scene("res://scenes/game_scene.tscn")
 
-
-func _on_How_To_Play_pressed():
-	get_tree().change_scene("res://scenes/how_to_play.tscn")
-
-
 func _on_endless_button_pressed():
 	GlobalVars.game_type = "endless_game"
-	get_tree().change_scene("res://scenes/endless_game.tscn")
+	get_tree().change_scene("res://scenes/endless_game.tscn")	
+
+func _on_how_to_play_pressed():
+	get_tree().change_scene("res://scenes/how_to_play.tscn")
