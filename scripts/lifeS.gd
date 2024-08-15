7ddd3e8ce4d1865df59d5fc5ba16e0e71377eb5a
Sayem <sayem@caffebd.org>
2024-08-15 15:24:21 +0600

    life_done

---
+++ scripts/lifeS.gd
@@ -0,0 +1,8 @@
+extends RigidBody2D
+
+var speed_range = Vector2(250,350)
+
+func _on_life_area_body_entered(body):
+	if body.is_in_group("player"):
+		GlobalSignals.emit_signal("life_update")
+		queue_free()
